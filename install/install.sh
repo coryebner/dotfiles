e523bb353ea025223e146ced2c095f0e1794a8b2
Cory Ebner <coryebner@gmail.com>
2022-07-11 01:32:06 -0600

    Added Powerlevel10k zsh theme

--- install/install.sh
+++ install/install.sh
@@ -10,10 +10,13 @@ mkdir -p ~/build
 sudo dnf update
 
 #Install packages
+install " util-linux-user"
+
 install "zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 install "tmux"
 install "tldr"
